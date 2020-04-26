1486b414add58db24fc00145247f98ee4186a5e8
proshunsuke <shunsuke0901@gmail.com>
2020-04-26 16:54:37 +0900

    fix sha256

--- Formula/colmsg.rb
+++ Formula/colmsg.rb
@@ -3,7 +3,7 @@ class Colmsg < Formula
   desc "A CLI tool for '欅坂/日向坂メッセージアプリ'."
   homepage "https://github.com/proshunsuke/colmsg"
   url "https://github.com/proshunsuke/colmsg/releases/download/v0.1.1/colmsg-v0.1.1-x86_64-apple-darwin.tar.gz"
-  sha256 "3ceac7a708003be9c4b06f4ac14b93db449675ab28c0aa64154027dfcf5125d5"
+  sha256 "f7ffab5d922b1a7909611d0095131eb51f0ac975b1eb39074b82999f740615f4"
 
   def install
     bin.install "colmsg"
