6d407fa1b820db870b62e399387ceb4852e53b13
proshunsuke <shunsuke0901@gmail.com>
2022-11-18 02:47:40 +0900

    colmsg version3.0.1

--- Formula/colmsg.rb
+++ Formula/colmsg.rb
@@ -2,8 +2,8 @@
 class Colmsg < Formula
   desc "A CLI tool for 櫻坂46メッセージ, 日向坂46メッセージ and 乃木坂46メッセージ app."
   homepage "https://github.com/proshunsuke/colmsg"
-  url "https://github.com/proshunsuke/colmsg/releases/download/v3.0.0/colmsg-v3.0.0-x86_64-apple-darwin.tar.gz"
-  sha256 "6d598bb355ae712ce60609499cd32fa452ad7d175e34e4e079fe9ee85daf42fb"
+  url "https://github.com/proshunsuke/colmsg/releases/download/v3.0.1/colmsg-v3.0.1-x86_64-apple-darwin.tar.gz"
+  sha256 "10565a92777fbbb4b9e0c4fac8b68cdb07b216b023e4229cdd446e821fc43bde"
 
   def install
     bin.install "colmsg"
