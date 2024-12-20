029dc5e6c721eae4ed96df5fa813cded27dc60c5
proshunsuke <shunsuke0901@gmail.com>
2024-12-21 00:54:04 +0900

    colmsg version3.2.1

--- Formula/colmsg.rb
+++ Formula/colmsg.rb
@@ -4,11 +4,11 @@ class Colmsg < Formula
   homepage "https://github.com/proshunsuke/colmsg"
 
   if Hardware::CPU.intel?
-    url "https://github.com/proshunsuke/colmsg/releases/download/v3.2.0/colmsg-v3.2.0-x86_64-apple-darwin.tar.gz"
-    sha256 "60385a212f349d577d0fb165986ff6ca3e44ceb9452c4caf82a5bea5927c4ddc"
+    url "https://github.com/proshunsuke/colmsg/releases/download/v3.2.1/colmsg-v3.2.1-x86_64-apple-darwin.tar.gz"
+    sha256 "87ad7226f28f555aee6b3113525989eb16ba2cd50a82a378637ea98c808de25c"
   elsif Hardware::CPU.arm?
-    url "https://github.com/proshunsuke/colmsg/releases/download/v3.2.0/colmsg-v3.2.0-aarch64-apple-darwin.tar.gz"
-    sha256 "b9e183c199add078723f290ff34759bbcbb496ea7f14cbbb190c510964000d87"
+    url "https://github.com/proshunsuke/colmsg/releases/download/v3.2.1/colmsg-v3.2.1-aarch64-apple-darwin.tar.gz"
+    sha256 "9c078fab1c181ded0cb8ee734c58006c83b2a0eb46951b1c786136e291cc3191"
   end
 
   def install
